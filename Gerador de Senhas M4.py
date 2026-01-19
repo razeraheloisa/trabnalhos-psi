3f85e0be255995509eb1a52d04954781c8ecb795
razeraheloisa <razeraheloisa58@gmail.com>
2026-01-19 13:11:04 +0000

    Modifiquei meu código
    Updated email validation to require '@gmail.com' and added a password confirmation loop to ensure user acceptance of generated password.

--- Gerador de Senhas M4.py
+++ Gerador de Senhas M4.py
@@ -4,30 +4,43 @@ print("=== CRIAR CONTA NO APLICATIVO ===")
 
 nome = input("Digite seu nome: ")
 
-
 email = input("Digite seu e-mail: ")
-while "@" not in email:
-    print("E-mail inválido. O e-mail precisa conter @")
+while "@gmail.com" not in email:
+    print("E-mail inválido. O e-mail precisa conter @gmail.com")
     email = input("Digite seu e-mail novamente: ")
 
 usuario = input("Digite um nome de usuário: ")
 
-
-palavra = input("Digite uma palavra para gerar sua senha: ")
-
 numeros = "0123456789"
 
-senha = palavra.lower()
+aceitou = False
 
-senha = senha[0].upper() + senha[1:]
+while not aceitou:
+    palavra = input("Digite uma palavra para gerar sua senha: ")
 
+    while len(palavra) < 4:
+        print("A palavra deve ter pelo menos 4 letras.")
+        palavra = input("Digite outra palavra: ")
 
-for i in range(3):
-    senha += random.choice(numeros)
+    senha = palavra.lower()
+    senha = senha[0].upper() + senha[1:]
+
+    for i in range(3):
+        senha += random.choice(numeros)
+
+    print("\nSenha gerada:", senha)
+    print("Tamanho da senha:", len(senha))
+
+    resposta = input("Aceita esta senha? (s/n): ")
+    if resposta.lower() == "s":
+        aceitou = True
+    else:
+        print("\nVamos criar uma nova senha.\n")
 
 print("\n=== CONTA CRIADA COM SUCESSO ===")
 print("Nome:", nome)
 print("E-mail:", email)
 print("Usuário:", usuario)
-print("Senha gerada:", senha)
-print("Tamanho da senha:", len(senha))
+
+print("\nMensagem de segurança:")
+print("Nunca partilhe a sua senha com outras pessoas.")
